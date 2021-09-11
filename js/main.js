6a6c7d405fca45d8b5f9a9f610ef06f1beb75969
skullr <50787557+skullr2518@users.noreply.github.com>
2021-09-11 17:52:15 +0300

    community section

--- js/main.js
+++ js/main.js
@@ -3,6 +3,15 @@ const tops = document.querySelector('.top')
 const nav_links = document.querySelector('.nav-links')
 const nav = document.querySelector('.nav')
 
+const big_image = document.querySelector('.big-image img')
+const small_images = document.querySelectorAll('.small-images img')
+
+small_images.forEach((img) => img.addEventListener('click', showImage))
+
+function showImage(e) {
+    big_image.src = e.target.src
+}
+
 menu.addEventListener('click', () => {
     nav_links.classList.toggle('open')
 })
