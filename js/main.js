f238dd879345dd9614176d90eac4defe458914ea
skullr <50787557+skullr2518@users.noreply.github.com>
2021-09-13 07:58:23 +0300

    community changes

--- js/main.js
+++ js/main.js
@@ -2,14 +2,17 @@ const menu = document.querySelector('.menu')
 const tops = document.querySelector('.top')
 const nav_links = document.querySelector('.nav-links')
 const nav = document.querySelector('.nav')
+const opacity = 0.3
 
 const big_image = document.querySelector('.big-image img')
 const small_images = document.querySelectorAll('.small-images img')
 
 small_images.forEach((img) => img.addEventListener('click', showImage))
+small_images[0].style.opacity = opacity
 
 function showImage(e) {
     big_image.src = e.target.src
+    small_images.forEach((img) => (img.style.opacity = 1))
 }
 
 menu.addEventListener('click', () => {
